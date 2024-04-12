56b424af13ae5c4c25f6076bd686e7b107f05fae
juninhojll <58446189+juninhojll@users.noreply.github.com>
2024-04-12 16:07:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
 function enviarDados() {
+    var d = new Date(); 
     var e, t = document.getElementById("nome").value,
         n = document.getElementById("idade").value,
         a = {
             nome: t,
             idade: n,
             nascimento: document.getElementById("nascimento").value,
-            endereco: document.getElementById("endereco").value
-            horario_clicado: new Date().toISOString()
+            endereco: document.getElementById("endereco").value,
+            h:d.getHours(),
+            m:d.getMinutes(),
+            s:s.getSeconds()
         },
-        s = CryptoJS.SHA256(a.horario_clicado).toString(CryptoJS.enc.Hex);
+        s = CryptoJS.SHA256(a.h + a.m + a.s).toString(CryptoJS.enc.Hex);
     a.token = s;
-    console.log(s)
+    console.log(a.h + a.m + a.s);
+    console.log("h",a.h);
+    console.log("m",a.m);
+    console.log("s",a.s);
+    console.log(s);
 
 }
