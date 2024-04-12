f916418ffd3dc3ef762a109b88e7a96d77664742
juninhojll <58446189+juninhojll@users.noreply.github.com>
2024-04-12 14:28:46 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,6 +16,22 @@ function enviarDados() {
       var token = CryptoJS.SHA256(dados.nascimento).toString(CryptoJS.enc.Hex);
       
       dados.token = token;
-      console.log(dados.nascimento);
-      console.log(dados.token);
+      var dadosAtualizadosJSON = JSON.stringify(dados);
+      var xhr = new XMLHttpRequest();
+      var statusElement = document.getElementById("status");
+      statusElement.textContent = "Enviando...";
+      xhr.open("POST", "https://eo17ntwnezhvaf.m.pipedream.net", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            statusElement.textContent = "Requisição enviada com sucesso!";
+            statusElement.className = "status success";
+          } else {
+            statusElement.textContent = "Ocorreu um erro ao enviar a requisição.";
+            statusElement.className = "status error";
+          }
+        }
+      };
+      xhr.send(dadosAtualizadosJSON);
 }
